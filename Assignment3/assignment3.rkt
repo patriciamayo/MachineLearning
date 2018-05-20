fe2fe9b20e6dc9b7faacc814b841ff21e6f3cbef
Patricia Mayo Tejedor <patricia.mayo@nl.ibm.com>
2018-05-20 16:43:41 +0200

    ejercicio 19

--- Assignment3/assignment3.rkt
+++ Assignment3/assignment3.rkt
@@ -1932,3 +1932,32 @@
          cl1 )
      ) concepto-CL1 concepto-CL2)
 ))
+
+
+; Ejercicio 19
+(define JC '(adc (((soleado)(*)) -> (adc (((*)(-inf.0 30)) -> (=> +)) (((*)((30) +inf.0)) -> (=> -)) )) (((nublado)(*)) -> (=> -)) (((lluvioso)(*)) -> (adc (((*)(-inf.0 10)) -> (=> -)) (((*)((10) +inf.0)) -> (=> +)) )) ))
+(define (DTL arbol-decision-clasico)
+(let* ()
+  (define recorrerArbol
+    (lambda(N RULES LHS)
+      (if (empty? N)
+          '()
+          (if (eq? (first N) '=>) ; Ver si es nodo terminal
+              (list (list* 'match-CL LHS N ))
+              (let* ()
+                (apply append (map
+                               (lambda(S)
+                                 (define T (first S))
+                                 (define nuevoLHS
+                                   (if (empty? LHS)
+                                       T
+                                       (union-CL T LHS)
+                                       ))
+                                 (recorrerArbol (list-ref S 2) RULES nuevoLHS)
+                                 ) (cdr N))))))
+      ))
+  (remove-duplicates (recorrerArbol arbol-decision-clasico '() '()))
+))
+
+(define (DTL-DDT ejemplos)
+(DTL (DDT ejemplos)))
--- Assignment3/b3-e18-e20.rkt
+++ Assignment3/b3-e18-e20.rkt
@@ -19,13 +19,41 @@
 ))
 
 ;;Ejercicio 19
-(he-tardado <minutos> 'b3-e19)
-;;<comentarios>
+(he-tardado 200 'b3-e19)
+;; He segudio el algoritmo de la tabla de EML, pero no termino de entender como se tienen que ordenar las RULES
+;(define JC '(adc (((soleado)(*)) -> (adc (((*)(-inf.0 30)) -> (=> +)) (((*)((30) +inf.0)) -> (=> -)) )) (((nublado)(*)) -> (=> -)) (((lluvioso)(*)) -> (adc (((*)(-inf.0 10)) -> (=> -)) (((*)((10) +inf.0)) -> (=> +)) )) ))
+;> (DTL JC)
+;'((match-CL ((soleado) (-inf.0 30)) => +)
+;  (match-CL ((soleado) ((30) +inf.0)) => -)
+;  (match-CL ((nublado) (*)) => -)
+;  (match-CL ((lluvioso) (-inf.0 10)) => -)
+;  (match-CL ((lluvioso) ((10) +inf.0)) => +))
+
 (define (DTL arbol-decision-clasico)
-<codigo>)
+(let* ()
+  (define recorrerArbol
+    (lambda(N RULES LHS)
+      (if (empty? N)
+          '()
+          (if (eq? (first N) '=>) ; Ver si es nodo terminal
+              (list (list* 'match-CL LHS N ))
+              (let* ()
+                (apply append (map
+                               (lambda(S)
+                                 (define T (first S))
+                                 (define nuevoLHS
+                                   (if (empty? LHS)
+                                       T
+                                       (union-CL T LHS)
+                                       ))
+                                 (recorrerArbol (list-ref S 2) RULES nuevoLHS)
+                                 ) (cdr N))))))
+      ))
+  (remove-duplicates (recorrerArbol arbol-decision-clasico '() '()))
+))
 
 (define (DTL-DDT ejemplos)
-<codigo>)
+(DTL (DDT ejemplos)))
 
 ;;Ejercicio 20
 (he-tardado <minutos> 'b3-e20)
