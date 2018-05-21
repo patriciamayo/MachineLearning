8d1671ffd0179ff5e4e480ee4e8edf20bf7d0e23
Patricia Mayo Tejedor <patricia.mayo@nl.ibm.com>
2018-05-21 17:58:41 +0200

    comentarios bloque 2, 6-8

--- Assignment2/b2-e6-e8.rkt
+++ Assignment2/b2-e6-e8.rkt
@@ -3,8 +3,44 @@
 
 
 ;;Ejercicio 6
-(he-tardado 210 'b2-e6)
-;;<comentarios>
+(he-tardado 300 'b2-e6)
+
+;; IMPORTANTE: no sabia si se tenia que tener en cuenta la amplitud del rango
+;; en el caso de test numericos, mismo numeros en el rango, pero si es cerrado abarca mas que abierto
+;; Si se debe tener en cuenta descomentar la parte del segun el rango
+
+;; Este ejercicio ha sido muy muy complicado y lioso
+;; Los ejemplos no aclaran mucho cuantas categorias hay por lo que hay veces que no se entiende
+;; Habia un adjunto de aclaraciones en "Material de Estudio" que me termino por liar muchisimo mas
+;; He ido haciendo cambios segun continuaba luego con otros ejercicios
+
+; A mi entender hay cuatro categorias, de general a mas especifica:
+;    (*) que acepta todo
+;    (valor) que acepta sólo el valor
+;    (min max) que acepta un rango de valores entre min y max
+;    () que no acepta nada
+
+;>  (test-CL>= '() '())
+;#t
+;>  (test-CL>= '() '(lluvia))
+;#f
+;>  (test-CL>= '(lluvia) '())
+;#t
+;>  (test-CL>= '(lluvia) '(soleado))
+;#t
+;> (test-CL>= '(lluvia) '(*))
+;#f
+;> (test-CL>= '(*) '(lluvia))
+;#t
+;> (test-CL>= '(*) '(*))
+;#t
+;> (test-CL>= '(25 30) '(26))
+;#t
+;> (test-CL>= '(25 30) '(21 25))
+;#t
+;> (test-CL>= '(26) '(25 30))
+;#f
+
 (define atributoNominal
     (lambda (t1 t2)
       (cond
@@ -34,13 +70,16 @@
       )
   (define pasaTestNominal
    (lambda (t1 t2)
-          (cond
+     (cond
             ; Primero nos aseguramos que podemos usar car
-            [(and (pair? t1) (pair? t2) (symbol? (car t1)) (symbol? (car t2))) #t]
+            ;[(and (pair? t1) (pair? t2) (symbol? (car t1)) (symbol? (car t2))) #t]
             ; Si pasa el test nominal significa que es igual o mas general
             [(testNominal t1 t2) #t] 
             [(eq? t2 empty) #t]
-            [else #f])))
+            [(equal? t2 (list '*)) #f]
+            [(and (pair? t1) (pair? t2) (symbol? (car t1)) (symbol? (car t2))) #t]
+            [else #f])
+     ))
        (define pasaTestNumerico
          (lambda (t1 t2)
            (cond
@@ -52,6 +91,18 @@
              [(> (length t1) (length t2)) #t]
              [(< (length t1) (length t2)) #f]
              ;[(and (number? (car t1)) (number? (car t2))) #t]
+
+
+
+             ;; ============= IMPORTANTE ==========
+             ; Si esto deberia dar false 
+             ;> (test-CL>= '(25 30) '(25 (30)))
+             ;#f
+             ;descomentar la siguiente parte de amplitud de rango
+             ;; ======================================
+
+
+             
              ; Segun amplitud de rango
              ;[(> (- (valor (list-ref t1 1)) (valor (car t1)))
              ;    (- (valor (list-ref t2 1)) (valor (car t2)))) #t]
@@ -61,9 +112,16 @@
       (if esTestNominal (pasaTestNominal t1 t2) (pasaTestNumerico t1 t2))
     ))
 
+
 ;;Ejercicio 7
 (he-tardado 60 'b2-e7)
 ;; Tenia errores en el ejercicio anterior asique he tenido que rehacerlo
+;> (concepto-CL>= '((lluvioso)(2 10)) '((soleado)(10 20)))
+;#t
+;> (concepto-CL>= '((lluvioso)(2 35)) '((soleado)(23)))
+;#t
+;> (concepto-CL>= '((soleado)(23)) '((lluvioso)(2 35)))
+;#f
 (define (concepto-CL>= c1 c2)
     (andmap (lambda (concepto1 concepto2)
                                 (test-CL>= concepto1 concepto2)) c1 c2))
@@ -72,6 +130,14 @@
 (he-tardado 30 'b2-e8)
 ;; De nuevo me he dado cuenta que tenia mal el ejercicio 6
 ;; Ya que para evaluar el grado de generalizacion estaba mirando la amplitud del rango
+
+;> (cmp-concepto-CL '((lluvioso)(2 35)) '((soleado)(23)))
+;1
+;> (cmp-concepto-CL '((lluvioso)(2 10)) '((soleado)(10 20)))
+;0
+;> (cmp-concepto-CL '((soleado)(23)) '((lluvioso)(2 35)))
+;-1
+
 (define (cmp-concepto-CL c1 c2)
     (cond
       [(and (concepto-CL>= c1 c2) (not (concepto-CL>= c2 c1))) 1]
